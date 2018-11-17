aeb7b13b84b4a47ecdc3a2bd68b60c0a606d52de
Eddie Padin <eddie@entertainermedia.net>
2018-11-17 16:35:36 -0600

    Updates

---
+++ precache-manifest.1bcfe8c0a779ff853f566f2e1d834fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23b2d069d95a08146c09",
+    "url": "/react-todos/static/js/runtime~main.23b2d069.js"
+  },
+  {
+    "revision": "70ee9fd099b071fa807b",
+    "url": "/react-todos/static/js/main.70ee9fd0.chunk.js"
+  },
+  {
+    "revision": "bb130b21128c2220b45c",
+    "url": "/react-todos/static/js/1.bb130b21.chunk.js"
+  },
+  {
+    "revision": "70ee9fd099b071fa807b",
+    "url": "/react-todos/static/css/main.caf9a4fa.chunk.css"
+  },
+  {
+    "revision": "bb130b21128c2220b45c",
+    "url": "/react-todos/static/css/1.a5a91039.chunk.css"
+  },
+  {
+    "revision": "d1f414ffa1f216674b241fd168334cfa",
+    "url": "/react-todos/index.html"
+  }
+];
--- src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-import logger from "./checker";
-import thunk from "redux-thunk";
-import { applyMiddleware } from "redux";
-
-/**
- * Middleware
- */
-export default applyMiddleware(thunk, logger);
