09a7c3f1c73f52c1048e0275a625846956972efe
Eddie Padin <eddie@entertainermedia.net>
2018-11-17 16:20:35 -0600

    fix: nav component; - add nav component;

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from "../actions/shared";
 import { Slide, ToastContainer } from "react-toastify";
 import Todos from "./Todos";
 import Goals from "./Goals";
+import Nav from "./Nav";
 
 class App extends Component {
   componentDidMount() {
@@ -13,42 +14,8 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Todo App
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Todos
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Goals
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="container">
+        <Nav />
+        <div className="container my-3">
           <Todos />
           <Goals />
         </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-brand">Todo App</span>
+    </nav>
+  );
+};
+
+export default Nav;
