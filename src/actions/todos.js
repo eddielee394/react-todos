5946b26b016816e596ff845ca609b06dc735b0c8
Eddie Padin <eddie@entertainermedia.net>
2018-11-17 11:50:07 -0600

    feat:  add todo actions & async action creators

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,13 +1,17 @@
 import API from 'goals-todos-api';
 
-//First we export the actions constants
+/**
+ * Global Constants
+ * @type {string}
+ * @constant
+ */
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 
 /**
- * Add's Todo
- * @type Action
+ * Add Todo
+ * @summary Action
  * @param todo
  * @return {{type: string, todo: *}}
  */
@@ -18,12 +22,25 @@ function addTodo(todo) {
     };
 }
 
+/**
+ * Remove Todo
+ * @summary Action
+ * @param id
+ * @return {{type: string, id: *}}
+ */
 function removeTodo(id) {
     return {
         type: REMOVE_TODO,
         id
     };
 }
+
+/**
+ * Toggle Todo
+ * @summary Action
+ * @param id
+ * @return {{type: string, id: *}}
+ */
 function toggleTodo(id) {
     return {
         type: TOGGLE_TODO,
@@ -31,7 +48,15 @@ function toggleTodo(id) {
     };
 }
 
-//Lastly we add the action creators
+
+/**
+ * Async Add Todo handler
+ * @summary Action Creator
+ * @async
+ * @param name
+ * @param callback
+ * @return {function(*): Promise<T | never>}
+ */
 export const handleAddTodo = (name, callback=null) => (dispatch) => {
     //pass the input value to get the item from the api
     return API.saveTodo(name)
@@ -50,6 +75,13 @@ export const handleAddTodo = (name, callback=null) => (dispatch) => {
     })
 };
 
+/**
+ * Async Delete Todo handler
+ * @summary Action Creator
+ * @param todo
+ * @return {function(*): Promise<T | never>}
+ * @async
+ */
 export const handleDeleteTodo = (todo) => (dispatch) => {
     //first we update the state so the UI changes instantly
     dispatch(removeTodo(todo.id));
@@ -64,6 +96,13 @@ export const handleDeleteTodo = (todo) => (dispatch) => {
     })
 };
 
+/**
+ * Async Toggle Todo handler
+ * @summary Action Creator
+ * @param id
+ * @return {function(*): Promise<T | never>}
+ * @async
+ */
 export const handleToggleTodo = (id) => (dispatch) => {
     //first we update the state so the UI changes instantly
     dispatch(toggleTodo(id));
